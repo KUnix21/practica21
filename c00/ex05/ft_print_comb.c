6eca6ca2845a697159b89791a3244f38d33fdf16
eslcg <emilio.eslc@gmail.com>
2021-07-28 12:42:36 +0300

    se agrego ejercicio 5

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -19,16 +19,16 @@ void ft_print_comb(void)
 		d='1';
 		while(d<='8')
 		{
-			u='2';
+			u=c+1;
 			while(u<='9')
 			{
 			
-				if(c<d && d<u)
+				if(c!=d && c!=u && d!=u)
 				{
 					write(1,&c,1);
 					write(1,&d,1);	
 					write(1,&u,1);
-					if(c<'7')
+					if(c!='7' || d!='8' || u!='9')
 					{	
 						write(1,",",1);
 						write(1," ",1);
